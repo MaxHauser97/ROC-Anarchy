fd8596b349ceb095064605f826966670d3e42925
Kevin <k.wouda@applicationbuilders.nl>
2016-07-12 14:34:13 +0200

    admin only, just for safety measures

--- CreateDatabases.php
+++ CreateDatabases.php
@@ -1,21 +1,26 @@
 <?php
-	//include 'Config.php';
-	
-	if (environment == "dev") {
-	
-		if ($conn = mysqli_connect(databasehost, databaseuser, databasepass)) {
-			echo "PREPARE, FOR SOME BLACK MAGIC!<br>";
-			echo CreateDatabase('rocanarchy');
-			echo CreateTable('users (id int AUTO_INCREMENT PRIMARY KEY, name varchar(255), password varchar(255))');
-			echo InsertData("INSERT INTO users (name, password) VALUES ('leerling', 'b9d71fbbd8ca839533a47180a57da24f0187f2ec55dd282fb90150686336980922a502949f36bffcd54d832ff373a377acbfe9a7480493a9a83f1122d6846d2b')");
-			echo InsertData("INSERT INTO users (name, password) VALUES ('admin', '6d83fb6436ca42f5283d85c8c186c2d780aae6b6325dd17adbaaab28d77edf54068197ca5cc652a85ff2b104f597f3de13340eaf97edc6253a7f250142b8f14f')");
-			echo CreateTable('updates (id int AUTO_INCREMENT PRIMARY KEY, title varchar(255), text varchar(255))');
-			echo InsertData("INSERT INTO updates (title, text) VALUES ('Test', 'Hier komen in de toekomst updates over de site te staan.')");
+	session_start();
+	if (isset($_SESSION["username"]) && $_SESSION["username"] == "admin") {
+		
+		//include 'Config.php';
+		
+		if (environment == "dev") {
+		
+			if ($conn = mysqli_connect(databasehost, databaseuser, databasepass)) {
+				echo "PREPARE, FOR SOME BLACK MAGIC!<br>";
+				echo CreateDatabase('rocanarchy');
+				echo CreateTable('users (id int AUTO_INCREMENT PRIMARY KEY, name varchar(255), password varchar(255))');
+				echo InsertData("INSERT INTO users (name, password) VALUES ('leerling', 'b9d71fbbd8ca839533a47180a57da24f0187f2ec55dd282fb90150686336980922a502949f36bffcd54d832ff373a377acbfe9a7480493a9a83f1122d6846d2b')");
+				echo InsertData("INSERT INTO users (name, password) VALUES ('admin', '6d83fb6436ca42f5283d85c8c186c2d780aae6b6325dd17adbaaab28d77edf54068197ca5cc652a85ff2b104f597f3de13340eaf97edc6253a7f250142b8f14f')");
+				echo CreateTable('updates (id int AUTO_INCREMENT PRIMARY KEY, title varchar(255), text varchar(255))');
+				echo InsertData("INSERT INTO updates (title, text) VALUES ('Test', 'Hier komen in de toekomst updates over de site te staan.')");
+			}
+			else {
+				echo "Wrong SQL login credentials. Edit your credentials in Config.php!";
+			}
+		
 		}
-		else {
-			echo "Wrong SQL login credentials. Edit your credentials in Config.php!";
-		}
-	
+		
 	}
 	
 	function CreateDatabase ($name) {
