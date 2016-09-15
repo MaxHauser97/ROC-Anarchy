1e52fe49b54174c2f77f69f5f8c4c4eafdd6443a
Kevin <k.wouda@applicationbuilders.nl>
2016-09-15 14:32:00 +0200

    Fix site icons

--- Userpage.php
+++ Userpage.php
@@ -52,10 +52,10 @@
 	<title>ROCAnarchy - Dashboard</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="http://st27.nl/siteIcon.png">
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link rel="icon" href="<?php if (usesSSL) {echo "https";} else {echo "http";} ?>://st27.nl/School/siteIcon.png">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<style>
 			body{background-color: #f5f5f5;}
 			.roc{color: #EF5350;}
