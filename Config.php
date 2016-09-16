f26e1b03a8bb670c8b5d413d149669f481eadb13
Kevin <k.wouda@applicationbuilders.nl>
2016-09-16 14:27:09 +0200

    Allow the blockage of browsers loading the site as https, because they don't support the certificate authorirty yet. Also update CreateDatabases to create better tables.

--- Config.php
+++ Config.php
@@ -7,4 +7,7 @@
 	define("environment", "dev");
 	define("maintenance", 0); //0: No, 1: Yes
 	define("usesSSL", true); //Does your site have SSL? (https)
+	//An array of browser names wich should be ignored for going to https because their authorities have not yet been updated
+	//Accepts: Chrome, Firefox, Internet Explorer, Edge, Opera, Safari and Other
+	define("ignoreSSLBrowser", array('Firefox'));
 ?>
--- SecurityLayer.php
+++ SecurityLayer.php
@@ -1,9 +1,40 @@
 <?php
-	if (usesSSL) {
+	$ignoreSSL = false;
+	foreach (ignoreSSLBrowser as $browser) {
+		if (get_browserName($_SERVER['HTTP_USER_AGENT']) == $browser) {
+			$ignoreSSL = true;
+		}
+	}
+	
+	if (usesSSL && !$ignoreSSL) {
 		if ($_SERVER['HTTPS'] != "on" && $_SERVER['SERVER_NAME'] != "localhost") { //Localhost doesn't require authentication, this would cause issues with local setups
 			$url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 			header("Location: $url");
 			exit;
 		}
+	}
+	
+	function get_browserName ($userAgent) {
+		if (strpos($userAgent, "Chrome")) {
+			return 'Chrome';
+		}
+		elseif(strpos($userAgent, "Firefox")) {
+			return 'Firefox';
+		}
+		elseif(strpos($userAgent, "MSIE") || strpos($userAgent, "Trident/7")) {
+			return 'Internet Explorer';
+		}
+		elseif(strpos($userAgent, "Edge")) {
+			return 'Edge';
+		}
+		elseif(strpos($userAgent, "Opera") || strpos($userAgent, "OPR/")) {
+			return 'Opera';
+		}
+		elseif(strpos($userAgent, "Safari")) {
+			return 'Safari';
+		}
+		
+		//If we don't know the browser, return 'Other', all other browsers can be blocked if so wished.
+		return 'Other';
 	}
 ?>
